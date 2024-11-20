c6338aeaa913c0c634ff10f92a7962eb760c820c
Zedran <17592740+Zedran@users.noreply.github.com>
2024-11-20 02:22:45 +0100

    utils.hpp: add omitted const keywords

--- src/utils.hpp
+++ src/utils.hpp
@@ -19,7 +19,7 @@ template <class F>
 
 // A helper function that appends the correct number of placeholders
 // to the sqlite statement in construction.
-std::string build_query_by_id_stmt(size_t length);
+std::string build_query_by_id_stmt(const size_t length);
 
 // Constructs a vector of all possible vendor identifiers that can be extracted
 // from addr. This is important in situations where user specifies
@@ -31,7 +31,7 @@ std::vector<int64_t> construct_queries(const std::string& addr);
 
 // Returns a vendor identifier block of length block_len extracted from addr.
 // If length of addr is lower than block_len, empty string is returned.
-std::string get_ieee_block(const std::string& addr, size_t block_len);
+std::string get_ieee_block(const std::string& addr, const size_t block_len);
 
 // Converts MAC prefix (10:10:10) from string to an integer.
 int64_t prefix_to_id(const std::string& prefix);
