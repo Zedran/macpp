1ee1d2bc2b229951a7512731bc0baff6ee588c88
Zedran <17592740+Zedran@users.noreply.github.com>
2024-11-20 19:56:26 +0100

    Improve Vendor test
    Add descriptions and new test cases.

--- tests/test_Vendor.cpp
+++ tests/test_Vendor.cpp
@@ -13,23 +13,37 @@ TEST_CASE("Vendor") {
         Vendor      expected;
     };
 
-    test_case cases[] = {
+    const test_case cases[] = {
         test_case{
+            // Quoted vendor name
             R"(00:00:0C,"Cisco Systems, Inc",false,MA-L,2015/11/17)",
             Vendor{"00:00:0C", "Cisco Systems, Inc", false, "MA-L", "2015/11/17"}
         },
         test_case{
+            // Non-quoted vendor name
             R"(00:00:0D,FIBRONICS LTD.,false,MA-L,2015/11/17)",
             Vendor{"00:00:0D", "FIBRONICS LTD.", false, "MA-L", "2015/11/17"}
         },
         test_case{
+            // Quoted vendor name, longer prefix
             R"(5C:F2:86:D,"BrightSky, LLC",false,MA-M,2019/07/02)",
             Vendor{"5C:F2:86:D", "BrightSky, LLC", false, "MA-M", "2019/07/02"}
         },
         test_case{
+            // Non-quoted vendor name, longer prefix
             R"(8C:1F:64:F5:A,Telco Antennas Pty Ltd,false,MA-S,2021/10/13)",
             Vendor{"8C:1F:64:F5:A", "Telco Antennas Pty Ltd", false, "MA-S", "2021/10/13"}
         },
+        test_case{
+            // Escaped quotes inside quoted vendor name
+            R"(2C:7A:FE,"IEE&E ""Black"" ops",false,MA-L,2010/07/26)",
+            Vendor{"2C:7A:FE", "IEE&E \"Black\" ops", false, "MA-L", "2010/07/26"},
+        },
+        test_case{
+            // Private block
+            R"(00:48:54,,true,,0001/01/01)",
+            Vendor{"00:48:54", "", true, "", "0001/01/01"},
+        }
     };
 
     for (auto& c : cases) {
