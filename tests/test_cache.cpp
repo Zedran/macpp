e0a86f3ac028e75aba299f412a22e96fb9202164
Zedran <17592740+Zedran@users.noreply.github.com>
2024-11-28 20:45:05 +0100

    Test injections: use testdata/sample.db instead of in-memory database

--- tests/test_cache.cpp
+++ tests/test_cache.cpp
@@ -5,34 +5,6 @@
 #include "FinalAction.hpp"
 #include "cache.hpp"
 
-// Creates an in-memory database for testing cache. Returns sqlite3 code.
-int create_test_cache(sqlite3*& conn) {
-    const char* create_table_stmt =
-        R"(CREATE TABLE vendors (
-            id      INTEGER PRIMARY KEY,
-            addr    TEXT NOT NULL,
-            name    TEXT NOT NULL,
-            private BOOLEAN,
-            block   TEXT NOT NULL,
-            updated TEXT NOT NULL
-        );
-        INSERT INTO vendors (id, addr, name, private, block, updated)
-        VALUES
-        (12, '00:00:0C', 'Cisco Systems, Inc', false, 'MA-L', '2015/11/17'),
-        (12876445, '0C:47:A9:D', 'DIG_LINK', false, 'MA-M', '2024/10/28');
-    )";
-
-    int code = -1;
-
-    if ((code = sqlite3_open(":memory:", &conn)) != SQLITE_OK)
-        return code;
-
-    if ((code = sqlite3_exec(conn, create_table_stmt, nullptr, nullptr, nullptr)) != SQLITE_OK)
-        return code;
-
-    return code;
-}
-
 TEST_CASE("get_conn") {
     sqlite3_initialize();
 
@@ -60,15 +32,7 @@ TEST_CASE("injections") {
         sqlite3_shutdown();
     });
 
-    int code;
-
-    if ((code = create_test_cache(conn)) != SQLITE_OK) {
-        FAIL(std::format(
-            "create_test_cache failed: ({}) {}",
-            code,
-            static_cast<std::string>(sqlite3_errmsg(conn))
-        ));
-    }
+    get_conn(conn, "testdata/sample.db");
 
     const std::string cases[] = {
         "DIG_LINK",
