d4d728636c86fe830d529a8fc5e4905386099de7
Zedran <17592740+Zedran@users.noreply.github.com>
2024-12-26 00:35:05 +0100

    Add missing checksums for static dependencies

--- cmake/argparse.cmake
+++ cmake/argparse.cmake
@@ -4,5 +4,6 @@ FetchContent_Declare(
 	argparse
 	DOWNLOAD_EXTRACT_TIMESTAMP OFF
 	URL https://github.com/p-ranav/argparse/archive/refs/tags/${ARGPARSE_VERSION}.tar.gz
+	URL_HASH SHA256=d01733552ca4a18ab501ae8b8be878131baa32e89090fafdeef018ebfa4c6e46
 )
 FetchContent_MakeAvailable(argparse)
--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -5,6 +5,7 @@ if (NOT Catch2_FOUND)
 		Catch2
 		DOWNLOAD_EXTRACT_TIMESTAMP OFF
 		URL https://github.com/catchorg/Catch2/archive/refs/tags/${CATCH2_VERSION}.tar.gz
+		URL_HASH SHA256=c991b247a1a0d7bb9c39aa35faf0fe9e19764213f28ffba3109388e62ee0269c
 	)
 	FetchContent_GetProperties(Catch2)
 	if (NOT Catch2_POPULATED)
