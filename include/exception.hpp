47e6394551f16f625f1d227e638fcf9276da1fd9
Zedran <17592740+Zedran@users.noreply.github.com>
2024-12-29 02:33:40 +0100

    Add a new CacheError class that represents sqlite errors

--- include/exception.hpp
+++ include/exception.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <format>
 #include <iostream>
+#include <sqlite3.h>
 #include <stdexcept>
 
 namespace errors {
@@ -9,8 +11,42 @@ class Error : public std::runtime_error {
     using std::runtime_error::runtime_error;
 
 public:
+    bool operator==(const Error& other) const noexcept {
+        return std::string(this->what()) == std::string(other.what());
+    }
+
+    bool operator!=(const Error& other) const noexcept {
+        return std::string(this->what()) != std::string(other.what());
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Error& e) {
         return os << e.what();
+    }
+};
+
+class CacheError : public Error {
+    using Error::Error;
+
+public:
+    // Returns a new CacheError that wraps sqlite3 error information.
+    CacheError wrap(sqlite3* const conn) const {
+        if (conn) {
+            return CacheError(std::format(
+                "{}: ({}) {}",
+                this->what(),
+                sqlite3_errcode(conn),
+                sqlite3_errmsg(conn)
+            ));
+        }
+        return *this;
+    }
+
+    // Returns a new CacheError that wraps a custom message.
+    CacheError wrap(const char* message) const {
+        if (message) {
+            return CacheError(std::format("{}: {}", this->what(), message));
+        }
+        return *this;
     }
 };
 
---
+++ tests/test_exception.cpp
@@ -0,0 +1,41 @@
+#include <catch2/catch_test_macros.hpp>
+#include <sqlite3.h>
+
+#include "FinalAction.hpp"
+#include "exception.hpp"
+#include "internal/internal.hpp"
+
+TEST_CASE("CacheError") {
+    sqlite3_initialize();
+
+    sqlite3* conn{};
+    internal::open_mem_db(conn);
+
+    const auto cleanup = finally([&] {
+        sqlite3_close(conn);
+        sqlite3_shutdown();
+    });
+
+    const errors::CacheError BASE("sqlite3 error");
+
+    sqlite3_exec(conn, "A", nullptr, nullptr, nullptr);
+    errors::CacheError out = BASE.wrap(conn);
+    errors::CacheError expected("sqlite3 error: (1) near \"A\": syntax error");
+    CAPTURE(out);
+    REQUIRE(out == expected);
+
+    CAPTURE("nullptr conn");
+    out      = BASE.wrap(static_cast<sqlite3*>(nullptr));
+    expected = BASE;
+    REQUIRE(out == expected);
+
+    CAPTURE("message");
+    out      = BASE.wrap("boom");
+    expected = errors::CacheError("sqlite3 error: boom");
+    REQUIRE(out == expected);
+
+    CAPTURE("nullptr message");
+    out      = BASE.wrap(static_cast<const char*>(nullptr));
+    expected = BASE;
+    REQUIRE(out == expected);
+}
