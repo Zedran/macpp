fa8423793d0a7f147ddc157f39dea3f2459a43cb
Zedran <17592740+Zedran@users.noreply.github.com>
2025-01-07 02:50:15 +0100

    Provide sqlite3 for benchmarks and tests in bundled configuration

--- cmake/sqlite3.cmake
+++ cmake/sqlite3.cmake
@@ -7,25 +7,30 @@ if (BUNDLE)
 		URL_HASH SHA256=aa73d8748095808471deaa8e6f34aa700e37f2f787f4425744f53fdd15a89c40
 	)
 	FetchContent_MakeAvailable(sqlite3)
-
-	target_sources(${PROJECT_NAME} PRIVATE ${sqlite3_SOURCE_DIR}/sqlite3.c)
-	target_include_directories(${PROJECT_NAME} PRIVATE ${sqlite3_SOURCE_DIR})
-
-	target_compile_definitions(${PROJECT_NAME} PRIVATE
-		SQLITE_DQS=0
-		SQLITE_THREADSAFE=0
-		SQLITE_DEFAULT_MEMSTATUS=0
-		SQLITE_DEFAULT_WAL_SYNCHRONOUS=0
-		SQLITE_LIKE_DOESNT_MATCH_BLOBS
-		SQLITE_MAX_EXPR_DEPTH=0
-		SQLITE_OMIT_DECLTYPE
-		SQLITE_OMIT_DEPRECATED
-		SQLITE_OMIT_PROGRESS_CALLBACK
-		SQLITE_OMIT_SHARED_CACHE
-		SQLITE_USE_ALLOCA
-		SQLITE_OMIT_AUTOINIT
-	)
 else()
 	find_package(SQLite3 REQUIRED)
-	target_link_libraries(${PROJECT_NAME} PRIVATE SQLite::SQLite3)
 endif()
+
+function(add_sqlite3 target_name)
+	if (BUNDLE)
+		target_sources(${target_name} PRIVATE ${sqlite3_SOURCE_DIR}/sqlite3.c)
+		target_include_directories(${target_name} PRIVATE ${sqlite3_SOURCE_DIR})
+
+		target_compile_definitions(${target_name} PRIVATE
+			SQLITE_DQS=0
+			SQLITE_THREADSAFE=0
+			SQLITE_DEFAULT_MEMSTATUS=0
+			SQLITE_DEFAULT_WAL_SYNCHRONOUS=0
+			SQLITE_LIKE_DOESNT_MATCH_BLOBS
+			SQLITE_MAX_EXPR_DEPTH=0
+			SQLITE_OMIT_DECLTYPE
+			SQLITE_OMIT_DEPRECATED
+			SQLITE_OMIT_PROGRESS_CALLBACK
+			SQLITE_OMIT_SHARED_CACHE
+			SQLITE_USE_ALLOCA
+			SQLITE_OMIT_AUTOINIT
+		)
+	else()
+		target_link_libraries(${target_name} PRIVATE SQLite::SQLite3)
+	endif()
+endfunction()
