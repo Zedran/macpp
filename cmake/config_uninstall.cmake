723276cf96b1a48a332b0fa6b4ac9f6028e1f4b6
Zedran <17592740+Zedran@users.noreply.github.com>
2025-01-23 16:07:23 +0100

    Configure uninstall target

---
+++ cmake/config_uninstall.cmake
@@ -0,0 +1,13 @@
+# Based on https://gitlab.kitware.com/cmake/community/-/wikis/FAQ#can-i-do-make-uninstall-with-cmake
+
+if(NOT TARGET uninstall)
+	configure_file(
+		"${SCRIPTS}/uninstall.cmake.in"
+		"${SCRIPTS}/uninstall.cmake"
+		IMMEDIATE
+		@ONLY
+	)
+	add_custom_target(uninstall
+		COMMAND ${CMAKE_COMMAND} -P ${SCRIPTS}/uninstall.cmake
+	)
+endif()
