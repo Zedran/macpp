8a43c4d10108e80ef6ab45b5bd3e7e9189fdf2a4
Zedran <17592740+Zedran@users.noreply.github.com>
2025-07-03 19:48:59 +0200

    Stmt destructor: rename variable code to rc

--- src/Stmt.cpp
+++ src/Stmt.cpp
@@ -9,8 +9,8 @@ Stmt::Stmt(sqlite3* const conn, const std::string& str_stmt)
     : prepare_rc{sqlite3_prepare_v2(conn, str_stmt.c_str(), -1, &stmt, nullptr)} {}
 
 Stmt::~Stmt() {
-    [[maybe_unused]] const int code = sqlite3_finalize(stmt);
-    assert(code == SQLITE_OK || code == SQLITE_NOTADB);
+    [[maybe_unused]] const int rc = sqlite3_finalize(stmt);
+    assert(rc == SQLITE_OK || rc == SQLITE_NOTADB);
 }
 
 int Stmt::bind(const int coln, const int64_t value) const noexcept {
