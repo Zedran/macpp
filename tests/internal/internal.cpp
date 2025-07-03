9937c451a86948d735c022b1389cd2911746ab88
Zedran <17592740+Zedran@users.noreply.github.com>
2025-07-03 18:09:45 +0200

    tests/internal: remove unused open_mem_db function

--- tests/internal/internal.cpp
+++ tests/internal/internal.cpp
@@ -2,28 +2,6 @@
 
 namespace internal {
 
-int open_mem_db(sqlite3*& conn) {
-    constexpr const char* create_table_stmt =
-        R"(CREATE TABLE vendors (
-            id      INTEGER PRIMARY KEY,
-            addr    TEXT NOT NULL,
-            name    TEXT NOT NULL,
-            private BOOLEAN,
-            block   TEXT NOT NULL,
-            updated TEXT NOT NULL
-        );
-    )";
-
-    int code = -1;
-
-    if ((code = sqlite3_open(":memory:", &conn)) != SQLITE_OK)
-        return code;
-
-    code = sqlite3_exec(conn, create_table_stmt, nullptr, nullptr, nullptr);
-
-    return code;
-}
-
 void print_vector(std::ostringstream& oss, const std::vector<int64_t>& vec) {
     for (size_t i = 0; i < vec.size(); i++)
         oss << vec[i] << (i != vec.size() - 1 ? " " : "");
