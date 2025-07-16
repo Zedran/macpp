d1d6146c532b0a5ccbbf83e2fa284cd78b44c072
Zedran <17592740+Zedran@users.noreply.github.com>
2025-07-16 23:54:33 +0200

    Add the Downloader class that handles cache update from remote source

---
+++ include/Downloader.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <mutex>
+#include <sstream>
+
+#include "Updater.hpp"
+
+// Class that provides data for cache update from a remote source.
+class Downloader : public Updater {
+    // Signals whether curl_global_init() function has been called.
+    static std::once_flag curl_init;
+
+    // CURL object.
+    CURL* curl;
+
+    // Data retrieved from source.
+    std::stringstream data;
+
+    // WRITEFUNCTION function for CURL.
+    static size_t write_data(void* buffer, size_t size, size_t nmemb, void* userp);
+
+public:
+    // Constructs a new Downloader instance and downloads data from url.
+    Downloader(const std::string& url);
+
+    Downloader(const Downloader&)            = delete;
+    Downloader& operator=(const Downloader&) = delete;
+
+    ~Downloader();
+
+    // Returns a reference to the wrapped stream.
+    std::istream& get() noexcept override final;
+};
---
+++ src/Downloader.cpp
@@ -0,0 +1,51 @@
+#include "Downloader.hpp"
+#include "exception.hpp"
+
+std::once_flag Downloader::curl_init{};
+
+Downloader::Downloader(const std::string& url) {
+    std::call_once(curl_init, [&] {
+        if (const CURLcode rc = curl_global_init(CURL_GLOBAL_DEFAULT); rc != CURLE_OK) {
+            throw errors::NetworkError{"curl_global_init failed", rc};
+        }
+    });
+
+    if (!(curl = curl_easy_init())) {
+        throw errors::NetworkError{"curl_easy_init failed"};
+    }
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+    curl_easy_setopt(curl, CURLOPT_MAXFILESIZE, MAX_FSIZE);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+
+    if (const CURLcode rc = curl_easy_perform(curl); rc != CURLE_OK) {
+        curl_easy_cleanup(curl);
+
+        if (rc == CURLE_FILESIZE_EXCEEDED) {
+            throw errors::NetworkError{"file size limit exceeded during download"};
+        }
+
+        throw errors::NetworkError{"curl_easy_perform failed", rc};
+    }
+}
+
+Downloader::~Downloader() {
+    if (curl) {
+        curl_easy_cleanup(curl);
+    }
+}
+
+std::istream& Downloader::get() noexcept {
+    return data;
+}
+
+size_t Downloader::write_data(void* buffer, size_t size, size_t nmemb, void* userp) {
+    std::stringstream* data = static_cast<std::stringstream*>(userp);
+
+    data->write(static_cast<char*>(buffer), static_cast<std::streamsize>(size * nmemb));
+    return size * nmemb;
+}
