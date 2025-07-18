de5a2a758d3853d2a0f716890b37b5b17acfb4db
Zedran <17592740+Zedran@users.noreply.github.com>
2025-07-18 20:20:50 +0200

    Toolchain: suppress compiler warnings for C dependencies

--- cmake/toolchain/gcc.cmake
+++ cmake/toolchain/gcc.cmake
@@ -1,10 +1,10 @@
 set(WARN   "-Wall -Wconversion -Wextra -Wpedantic")
 set(HARDEN "-fstack-protector-strong -fcf-protection=full -fstack-clash-protection -D_FORTIFY_SOURCE=2 -fPIE -pie")
 
-set(CMAKE_C_FLAGS_DEBUG            "-O0 -ggdb3 ${WARN}"               )
-set(CMAKE_C_FLAGS_RELEASE          "-O3 -DNDEBUG ${WARN} ${HARDEN}"   )
-set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -DNDEBUG ${WARN} ${HARDEN}")
-set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -DNDEBUG ${WARN} ${HARDEN}"   )
+set(CMAKE_C_FLAGS_DEBUG            "-O0 -ggdb3"                       )
+set(CMAKE_C_FLAGS_RELEASE          "-O3 -DNDEBUG ${HARDEN}"           )
+set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -DNDEBUG ${HARDEN}"        )
+set(CMAKE_C_FLAGS_MINSIZEREL       "-Os -DNDEBUG ${HARDEN}"           )
 
 set(CMAKE_CXX_FLAGS_DEBUG          "-O0 -ggdb3 ${WARN}"               )
 set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG ${WARN} ${HARDEN}"   )
--- cmake/toolchain/msvc.cmake
+++ cmake/toolchain/msvc.cmake
@@ -1,10 +1,10 @@
 set(WARN   "/W4 /WX- /permissive-")
 set(HARDEN "/GS /guard:cf /DYNAMICBASE /NXCOMPAT")
 
-set(CMAKE_C_FLAGS_DEBUG            "/Od /Zi ${WARN}"                   )
-set(CMAKE_C_FLAGS_RELEASE          "/O2 /DNDEBUG ${WARN} ${HARDEN}"    )
-set(CMAKE_C_FLAGS_RELWITHDEBINFO   "/Ox /Zi /DNDEBUG ${WARN} ${HARDEN}")
-set(CMAKE_C_FLAGS_MINSIZEREL       "/O1 /DNDEBUG ${WARN} ${HARDEN}"    )
+set(CMAKE_C_FLAGS_DEBUG            "/Od /Zi "                          )
+set(CMAKE_C_FLAGS_RELEASE          "/O2 /DNDEBUG ${HARDEN}"            )
+set(CMAKE_C_FLAGS_RELWITHDEBINFO   "/Ox /Zi /DNDEBUG ${HARDEN}"        )
+set(CMAKE_C_FLAGS_MINSIZEREL       "/O1 /DNDEBUG ${HARDEN}"            )
 
 set(CMAKE_CXX_FLAGS_DEBUG          "/Od /Zi ${WARN}"                   )
 set(CMAKE_CXX_FLAGS_RELEASE        "/O2 /DNDEBUG ${WARN} ${HARDEN}"    )
