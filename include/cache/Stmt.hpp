715bf793d0f9ca0a66051d79b922e441e7ecba2f
Zedran <17592740+Zedran@users.noreply.github.com>
2025-12-23 19:58:53 +0100

    Stmt::clear_bindings: improve comment

--- include/cache/Stmt.hpp
+++ include/cache/Stmt.hpp
@@ -37,7 +37,7 @@ public:
     // Empty string is bound as NULL.
     int bind(const int coln, const std::string& value) noexcept;
 
-    // Clears binding from the statement.
+    // Clears parameters that were bound to the statement.
     int clear_bindings() noexcept;
 
     // Returns pointer to the wrapped sqlite3_stmt object.
